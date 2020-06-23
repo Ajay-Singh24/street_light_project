eea8bac86daf8d20e32749c09b953ff3de86b01e
singha024 <62828638+singha024@users.noreply.github.com>
2020-06-23 11:33:29 +0100

    Update street_light_project.ino

--- street_light_project/street_light_project.ino
+++ street_light_project/street_light_project.ino
@@ -1,5 +1,4 @@
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(A0,INPUT);
   pinMode(D2,OUTPUT);
@@ -7,7 +6,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 int s=analogRead(A0);
 Serial.println(s);
 if (s<7){
